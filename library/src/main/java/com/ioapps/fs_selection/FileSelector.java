7efa788e08de52484a737f3da0dd00d1aea08537
ioapps-dev <37003408+ioapps-dev@users.noreply.github.com>
2018-03-12 20:59:12 +0100

    Add files via upload

---
+++ library/src/main/java/com/ioapps/fs_selection/FileSelector.java
@@ -0,0 +1,134 @@
+package com.ioapps.fs_selection;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+
+import java.util.ArrayList;
+
+public class FileSelector {
+
+    private final Activity context;
+    private String chooserTitle;
+    private int requestCode = 1000001;
+    private boolean showOnlyFs = true, mandatoryFs = false;
+
+    public FileSelector(Activity context) {
+        this.context = context;
+        chooserTitle = context.getString(R.string.select_chooser);
+    }
+
+    public String getChooserTitle() {
+        return chooserTitle;
+    }
+
+    public void setChooserTitle(String chooserTitle) {
+        this.chooserTitle = chooserTitle;
+    }
+
+    public int getRequestCode() {
+        return requestCode;
+    }
+
+    public void setRequestCode(int requestCode) {
+        this.requestCode = requestCode;
+    }
+
+    public void setShowOnlyFs(boolean showOnlyFs) {
+        this.showOnlyFs = showOnlyFs;
+    }
+
+    public void setMandatoryFs(boolean mandatoryFs) {
+        this.mandatoryFs = mandatoryFs;
+    }
+
+    public void chooseUniqueFile(String title, String subject) {
+        chooseFile(title, subject, "file/");
+    }
+
+    public void chooseUniqueFileByExtension(String title, String subject, String extension) {
+        chooseFile(title, subject, "file/" + extension);
+    }
+
+    public void chooseMultipleFiles(String title, String subject) {
+        chooseFile(title, subject, "file/multiple");
+    }
+
+    public void chooseMultipleFilesByExtension(String title, String subject, String extension) {
+        chooseFile(title, subject, "file/" + extension + "/multiple");
+    }
+
+    public void chooseUniqueFolder(String title, String subject) {
+        chooseFile(title, subject, "resource/folder");
+    }
+
+    public void chooseMultipleFolders(String title, String subject) {
+        chooseFile(title, subject, "folder/multiple");
+    }
+
+    public void chooseAnyUnique(String title, String subject) {
+        chooseFile(title, subject, "any/");
+    }
+
+    public void chooseAnyMultiple(String title, String subject) {
+        chooseFile(title, subject, "any/multiple");
+    }
+
+    /**
+     * Must be called from {@link Activity#onActivityResult(int, int, Intent)} of the involved
+     * activity.
+     * @return
+     * <br>an array with the selected elements
+     * <br>or an empty array if the operation has been canceled by the user
+     * <br>or null if the operation does not correspond
+     */
+    public Uri[] onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if(this.requestCode == requestCode) {
+            if(resultCode == Activity.RESULT_OK) {
+                return loadResult(intent);
+            } else {
+                return new Uri[] {};
+            }
+        }
+
+        return null;
+    }
+
+    public Uri[] loadResult(Intent intent) {
+        if(intent == null) {
+            return new Uri[] {};
+        }
+
+        Uri data = intent.getData();
+        if(data != null) {
+            return new Uri[] {data};
+        }
+
+        ArrayList<Uri> uriList = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+        return uriList.toArray(new Uri[uriList.size()]);
+    }
+
+    private void chooseFile(String title, String subject, String type) {
+        final Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.putExtra(Intent.EXTRA_TITLE, title);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.setType(type);
+
+        Intent chooserIntent = tryGetFsIntent(intent);
+
+        if(chooserIntent == null || !showOnlyFs) {
+            chooserIntent = FsUtils.createAppChooser(context.getPackageManager(), intent, chooserTitle, null);
+        }
+
+        context.startActivityForResult(chooserIntent, requestCode);
+    }
+
+    private Intent tryGetFsIntent(Intent intent) {
+        Intent fsIntent = FsUtils.getFsIntent(context.getPackageManager(), intent);
+        if(fsIntent == null) {
+            FsUtils.askUserInstall(context, mandatoryFs);
+        }
+
+        return fsIntent;
+    }
+}
---
+++ library/src/main/java/com/ioapps/fs_selection/FsUtils.java
@@ -0,0 +1,153 @@
+package com.ioapps.fs_selection;
+
+import android.app.AlertDialog;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.LabeledIntent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Parcelable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.CheckBox;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FsUtils {
+
+    private static final String FS_PACKAGE = "com.ioapps.fileselector";
+    private static final String FS_PREFS = "fs-prefs";
+    private static final String PREF_DONT_SHOW_INSTALL = "dont-show-install";
+
+    /*package*/ static Intent getFsIntent(PackageManager pm, Intent intent) {
+        ApplicationInfo appInfo = getAppInfo(pm, FS_PACKAGE);
+        if(appInfo == null) {
+            return null;
+        }
+
+
+        Intent fsIntent = new Intent(intent);
+        fsIntent.setPackage(FS_PACKAGE);
+        fsIntent.setComponent(new ComponentName(FS_PACKAGE, FS_PACKAGE + ".ChooserActivity"));
+        //return new LabeledIntent(newIntent, FS_PACKAGE, appInfo.labelRes, appInfo.icon);
+        return fsIntent;
+    }
+
+    /*package*/ static Intent createAppChooser(PackageManager pm, Intent intent, String title, String... patterns) {
+        List<LabeledIntent> intentList = getLabeledIntents(pm, intent, patterns);
+        Intent targetIntent = !intentList.isEmpty() ? intentList.remove(0) : intent;
+        Intent chooserIntent = Intent.createChooser(targetIntent, title);
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentList.toArray(new Parcelable[intentList.size()]));
+        return chooserIntent;
+    }
+
+    /*package*/ static boolean askUserInstall(Context context, boolean mandatory) {
+        final SharedPreferences prefs = FsUtils.getSharedPrefs(context);
+        boolean dontShowInstall = prefs.getBoolean(FsUtils.PREF_DONT_SHOW_INSTALL, false);
+
+        if(dontShowInstall && !mandatory) {
+            return false;
+        }
+
+        final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
+        final View view = LayoutInflater.from(context).inflate(R.layout.fs_install_dialog, null);
+        final CheckBox checkBoxNoShowMore = view.findViewById(R.id.checkBoxNoShowMore);
+
+        if(mandatory) {
+            checkBoxNoShowMore.setChecked(false);
+            checkBoxNoShowMore.setVisibility(View.GONE);
+        }
+
+        dialogBuilder.setTitle(R.string.required_action);
+        dialogBuilder.setView(view);
+
+        dialogBuilder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                // TODO ..
+            }
+        });
+
+        dialogBuilder.setPositiveButton(R.string.accept, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                boolean dontShowMore = checkBoxNoShowMore.isChecked();
+                prefs.edit().putBoolean(FsUtils.PREF_DONT_SHOW_INSTALL, dontShowMore).commit();
+            }
+        });
+
+        dialogBuilder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+                boolean dontShowMore = checkBoxNoShowMore.isChecked();
+                prefs.edit().putBoolean(FsUtils.PREF_DONT_SHOW_INSTALL, dontShowMore).commit();
+            }
+        });
+
+        dialogBuilder.show();
+        return true;
+    }
+
+    private static SharedPreferences getSharedPrefs(Context context) {
+        return context.getSharedPreferences(FS_PREFS, Context.MODE_PRIVATE);
+    }
+
+    private static ApplicationInfo getAppInfo(PackageManager pm, String packageName) {
+        try {
+            return pm.getApplicationInfo(packageName, 0);
+        } catch (Exception ex) {// PackageManager.NameNotFoundException
+        }
+
+        return null;
+    }
+
+    private static List<LabeledIntent> getLabeledIntents(PackageManager pm, Intent intent, String... patterns) {
+        List<ResolveInfo> riList = pm.queryIntentActivities(intent, 0);
+        Collections.sort(riList, new ResolveInfo.DisplayNameComparator(pm));
+        List<LabeledIntent> resultList = new ArrayList<>();
+
+        for (ResolveInfo ri : riList) {
+            String packageName = ri.activityInfo.packageName;
+
+            if (patterns != null && patterns.length > 0) {
+                boolean hasPattern = false;
+
+                for (String pattern : patterns) {
+                    if (packageName.contains(pattern)) {
+                        hasPattern = true;
+                        break;
+                    }
+                }
+
+                if (!hasPattern) {
+                    continue;
+                }
+            }
+
+            int labelId = ri.labelRes, iconId = ri.icon;
+
+            if (labelId == 0) {
+                labelId = ri.activityInfo.labelRes;
+            }
+
+            if (iconId == 0) {
+                iconId = ri.activityInfo.icon;
+            }
+
+            Intent newIntent = new Intent(intent);
+            newIntent.setPackage(packageName);
+            newIntent.setComponent(new ComponentName(packageName, ri.activityInfo.name));
+            resultList.add(new LabeledIntent(newIntent, packageName, labelId, iconId));
+        }
+
+        return resultList;
+    }
+}
